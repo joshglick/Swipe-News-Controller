f8da789ad6fe187db34e7457093de2e2771e0f76
Josh <joshglick@josh-glicks-macbook.local>
2012-02-26 14:08:56 -0500

    Initial Commit

---
+++ swipeNewsController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  swipeNewsController
+//
+//  Created by Josh Glick on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ swipeNewsController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  swipeNewsController
+//
+//  Created by Josh Glick on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
